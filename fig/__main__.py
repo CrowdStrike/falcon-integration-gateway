0a7b4eb37666f588f55bf755cc0aae95a337236d
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-03-23 13:17:56 +0000

    Wrap our configuration with custom class

--- fig/__main__.py
+++ fig/__main__.py
@@ -1,14 +1,14 @@
 from .falcon import StreamManagementThread
 from .gcp_translator import GCPWorkerThread
 from .queue import falcon_events
-from .config import validate_config
+from .config import config
 
 
 if __name__ == "__main__":
     # Central to the fig architecture is a message queue (falcon_events). GCPWorkerThread/s read the queue and process
     # each event. The events are put on queue by StreamingThread. StreamingThread is restarted by StreamManagementThread
 
-    validate_config()
+    config.validate()
 
     StreamManagementThread(output_queue=falcon_events).start()
     GCPWorkerThread(input_queue=falcon_events, daemon=True).start()  # TODO: Run multiple reader threads in pool
--- fig/config/__init__.py
+++ fig/config/__init__.py
@@ -2,30 +2,34 @@ import os
 import configparser
 
 
-config = configparser.SafeConfigParser()
-config.read(['config/defaults.ini', 'config/devel.ini', 'config/config.ini'])
+class FigConfig(configparser.SafeConfigParser):
+    ENV_DEFAULTS = [
+        ['falcon', 'cloud_region', 'FALCON_CLOUD_REGION'],
+        ['falcon', 'client_id', 'FALCON_CLIENT_ID'],
+        ['falcon', 'client_secret', 'FALCON_CLIENT_SECRET']
+    ]
+
+    def __init__(self):
+        super().__init__()
+        self.read(['config/defaults.ini', 'config/devel.ini', 'config/config.ini'])
+        self._override_from_env()
+
+    def _override_from_env(self):
+        for section, var, envvar in self.__class__.ENV_DEFAULTS:
+            value = os.getenv(envvar)
+            if value:
+                self.set(section, var, value)
+
+    def validate(self):
+        for section, var, envvar in self.__class__.ENV_DEFAULTS:
+            try:
+                self.get(section, var)
+            except configparser.NoOptionError as err:
+                raise Exception(
+                    "Please provide environment variable {} or configuration option {}.{}".format(
+                        envvar, section, var)) from err
+
+        assert int(self.get('events', 'severity_threshold')) in range(0, 5)
 
 
-__ENV_DEFAULTS = [
-    ['falcon', 'cloud_region', 'FALCON_CLOUD_REGION'],
-    ['falcon', 'client_id', 'FALCON_CLIENT_ID'],
-    ['falcon', 'client_secret', 'FALCON_CLIENT_SECRET']
-]
-
-
-for section, var, envvar in __ENV_DEFAULTS:
-    value = os.getenv(envvar)
-    if value:
-        config.set(section, var, value)
-
-
-def validate_config():
-    for section, var, envvar in __ENV_DEFAULTS:
-        try:
-            config.get(section, var)
-        except configparser.NoOptionError:
-            raise Exception(
-                "Please provide environment variable {} or configuration option {}.{}".format(
-                    envvar, section, var))
-
-    assert int(config.get('events', 'severity_threshold')) in range(0, 5)
+config = FigConfig()
