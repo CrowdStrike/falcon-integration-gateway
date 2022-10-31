cccd4bbfedf4920ba241f2d31cd834e20523fea3
Simon Lukasik <simon.lukasik@crowdstrike.com>
2022-10-31 12:00:23 +0000

    Log the FIG version early in the start up

--- fig/__main__.py
+++ fig/__main__.py
@@ -4,9 +4,13 @@ from .queue import falcon_events
 from .config import config
 from .backends import Backends
 from .falcon_data import FalconCache
+from .log import log
+from . import __version__
 
 
 if __name__ == "__main__":
+    log.info("Starting Falcon Integration Gateway %s", __version__)
+
     # Central to the fig architecture is a message queue (falcon_events). GCPWorkerThread/s read the queue and process
     # each event. The events are put on queue by StreamingThread. StreamingThread is restarted by StreamManagementThread
 
